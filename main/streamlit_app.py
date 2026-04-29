978638f91c31f2e7af4c48f93ca032bbbd7ca4c5
Jess <jessamae.sargento@gmail.com>
2026-04-29 22:25:29 +0100

    rev

--- main/streamlit_app.py
+++ main/streamlit_app.py
@@ -1,9 +1,9 @@
 import geopandas as gpd
 import folium
 import mapclassify
-from matploblib import pyplot as plt
+from matplotlib import pyplot as plt
 
 
-file = gpd.read_file('comarea\ComArea_ACS14.shp')
+file = gpd.read_file('comarea/ComArea_ACS14.shp')
 
 file.head()
