3fd8cad3dde4da660c8a64b7433344c02143d54c
Jess <jessamae.sargento@gmail.com>
2026-04-29 22:28:53 +0100

    Update streamlit_app.py

--- main/streamlit_app.py
+++ main/streamlit_app.py
@@ -2,8 +2,11 @@ import geopandas as gpd
 import folium
 import mapclassify
 from matplotlib import pyplot as plt
+import os
 
+# Build path relative to this script's location
+base_dir = os.path.dirname(__file__)
+file_path = os.path.join(base_dir, 'comarea', 'ComArea_ACS14.geojson')
 
-file = gpd.read_file('comarea/ComArea_ACS14.geojson')
-
+file = gpd.read_file(file_path)
 file.head()
