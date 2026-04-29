29da24334bf2340c2c964b99324f3f39e99a5351
Jess <jessamae.sargento@gmail.com>
2026-04-29 22:22:45 +0100

    add

--- main/streamlit_app.py
+++ main/streamlit_app.py
@@ -1,26 +1,9 @@
-import plotly.graph_objects as go
+import geopandas as gpd
+import folium
+import mapclassify
+from matploblib import pyplot as plt
 
-fig = go.Figure(go.Scattermap(
-        lat=['45.5017'],
-        lon=['-73.5673'],
-        mode='markers',
-        marker=go.scattermap.Marker(
-            size=14
-        ),
-        text=['Montreal'],
-    ))
 
-fig.update_layout(
-    hovermode='closest',
-    map=dict(
-        bearing=0,
-        center=go.layout.map.Center(
-            lat=45,
-            lon=-73
-        ),
-        pitch=0,
-        zoom=5
-    )
-)
+file = gpd.read_file('comarea\ComArea_ACS14.shp')
 
-fig.show()
+file.head()
